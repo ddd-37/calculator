a6ada30b622f55e8ac598c369c58ea0a2135681e
Devon Deason <devon.d.deason@gmail.com>
2019-12-02 17:20:57 -0700

    add click handlers

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,20 +5,56 @@ import BtnContainer from "./components/btns/BtnContainer";
 
 class Calculator extends React.Component {
   state = {
+    bttmDisplay: "0",
+    oprClick: false, // Use this to test if the last button clicked was an operator
     topDisplay: null,
-    bttmDisplay: "0"
+    total: null
   };
 
   handleBtnNumClick = num => {
     console.log("click");
-    let newBttmDisplay = this.state.bttmDisplay + `${num}`;
+
+    let newBttmDisplay = this.state.bttmDisplay;
 
     // Remove the first number if it's a zero
     if (newBttmDisplay.charAt(0) === "0") {
       newBttmDisplay = newBttmDisplay.substr(1);
+      console.log("TCL: Calculator -> newBttmDisplay", newBttmDisplay);
     }
 
-    this.setState({ bttmDisplay: newBttmDisplay });
+    // If an operator was the last click, we need a new bottom display
+    if (this.state.oprClick) {
+      this.setState({
+        bttmDisplay: newBttmDisplay,
+        oprClick: false
+      });
+      return;
+    }
+
+    newBttmDisplay = this.state.bttmDisplay + `${num}`;
+    this.setState({
+      bttmDisplay: newBttmDisplay,
+      oprClick: false
+    });
+  };
+
+  handleOperatorClick = opr => {
+    // If topDisplay is null, remove null and replace with value
+    let newTopDisplay;
+    if (!this.state.topDisplay) {
+      newTopDisplay = this.state.bttmDisplay + opr;
+      this.setState({
+        oprClick: true,
+        topDisplay: newTopDisplay
+      });
+      return;
+    }
+
+    newTopDisplay = this.state.topDisplay + opr;
+    this.setState({
+      oprClick: true,
+      topDisplay: newTopDisplay
+    });
   };
 
   render() {
@@ -28,7 +64,10 @@ class Calculator extends React.Component {
           bttmDisplay={this.state.bttmDisplay}
           topDisplay={this.state.topDisplay}
         />
-        <BtnContainer handleBtnNumClick={this.handleBtnNumClick} />
+        <BtnContainer
+          handleBtnNumClick={this.handleBtnNumClick}
+          handleOperatorClick={this.handleOperatorClick}
+        />
       </div>
     );
   }
--- src/components/btns/BtnContainer.js
+++ src/components/btns/BtnContainer.js
@@ -3,32 +3,57 @@ import "../../styles/BtnContainer.scss";
 import React from "react";
 import Btn from "./Btn";
 
-const BtnContainer = ({ handleBtnNumClick }) => {
+const BtnContainer = ({ handleBtnNumClick, handleOperatorClick }) => {
   return (
     <div className="btn__container">
       <div className="btn__row">
         <Btn type={"CE"} />
         <Btn type={"C"} />
-        <Btn type={"X"} />
-        <Btn type={"/"} />
+        <Btn
+          handleClick={() => {
+            handleOperatorClick("D");
+          }}
+          type={"D"}
+        />
+        <Btn
+          handleClick={() => {
+            handleOperatorClick("/");
+          }}
+          type={"/"}
+        />
       </div>
       <div className="btn__row">
         <Btn handleClick={() => handleBtnNumClick("7")} type={"7"} />
         <Btn handleClick={() => handleBtnNumClick("8")} type={"8"} />
         <Btn handleClick={() => handleBtnNumClick("9")} type={"9"} />
-        <Btn type={"X"} />
+        <Btn
+          handleClick={() => {
+            handleOperatorClick("X");
+          }}
+          type={"X"}
+        />
       </div>
       <div className="btn__row">
         <Btn handleClick={() => handleBtnNumClick("4")} type={"4"} />
         <Btn handleClick={() => handleBtnNumClick("5")} type={"5"} />
         <Btn handleClick={() => handleBtnNumClick("6")} type={"6"} />
-        <Btn type={"-"} />
+        <Btn
+          handleClick={() => {
+            handleOperatorClick("-");
+          }}
+          type={"-"}
+        />
       </div>
       <div className="btn__row">
         <Btn handleClick={() => handleBtnNumClick("1")} type={"1"} />
         <Btn handleClick={() => handleBtnNumClick("2")} type={"2"} />
         <Btn handleClick={() => handleBtnNumClick("3")} type={"3"} />
-        <Btn type={"+"} />
+        <Btn
+          handleClick={() => {
+            handleOperatorClick("+");
+          }}
+          type={"+"}
+        />
       </div>
       <div className="btn__row">
         <Btn type={"+/-"} />
